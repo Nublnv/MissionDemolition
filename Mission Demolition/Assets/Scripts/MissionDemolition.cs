aaf4bd0a42a1f8d6fc09d2cd782972c58deb61b2
Nublnv <cp7reg@gmail.com>
2021-03-16 14:10:45 +0300

    no message

---
+++ Mission Demolition/Assets/Scripts/MissionDemolition.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum GameMod
+{
+    idle,
+    playing,
+    levelEnd
+}
+
+public class MissionDemolition : MonoBehaviour
+{
+    static private MissionDemolition S;
+
+    [Header("Set in Inspector")]
+    public Text uitLevel;
+    public Text uitShots;
+    public Text uitButton;
+    public Vector3 castlePos;
+    public GameObject[] castles;
+
+    [Header("Set Dynamicaly")]
+    public int level;
+    public int levelMax;
+    public int shotsTaken;
+    public GameObject castle;
+    public GameMod mode = GameMod.idle;
+    public string showing = "Show Slingshot";
+    // Start is called before the first frame update
+    void Start()
+    {
+        S = this;
+
+        level = 0;
+        levelMax = castles.Length;
+        StartLevel();
+    }
+
+    void StartLevel()
+    {
+        if (castle != null)
+        {
+            Destroy(castle);
+        }
+        GameObject[] gos = GameObject.FindGameObjectsWithTag("Projectile");
+        foreach (GameObject tGO in gos)
+            Destroy(tGO);
+
+        castle = Instantiate(castles[level]);
+        castle.transform.position = castlePos;
+
+
+        SwitchView("Show Both");
+        ProjectileLine.S.Clear();
+
+        Goal.goalMet = false;
+
+        UpdateGUI();
+
+        mode = GameMod.playing;
+    }
+
+    void UpdateGUI()
+    {
+        uitLevel.text = "Level: " + (level + 1) + " of " + levelMax;
+        uitShots.text = "Shots taken: " + shotsTaken;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateGUI();
+
+        if ((mode == GameMod.playing) && Goal.goalMet)
+        {
+            mode = GameMod.levelEnd;
+            SwitchView("Show Both");
+            Invoke("NextLevel", 2f);
+        }    
+    }
+
+    void NextLevel()
+    {
+        level++;
+        if (level == levelMax)
+        {
+            level = 0;
+        }
+        StartLevel();
+    }
+
+    public void SwitchView(string eView = "")
+    {
+        if (eView == "")
+            eView = uitButton.text;
+        showing = eView;
+        switch(showing)
+        {
+            case "Show Slingshot":
+                FollowCam.POI = null;
+                uitButton.text = "Show Castle";
+                break;
+            case "Show Castle":
+                FollowCam.POI = S.castle;
+                uitButton.text = "Show Both";
+                break;
+            case "Show Both":
+                FollowCam.POI = GameObject.Find("ViewBoth");
+                uitButton.text = "Show Slingshot";
+                break;
+        }
+    }
+
+    public static void ShotFired()
+    {
+        S.shotsTaken++;
+    }
+}
---
+++ Mission Demolition/Assets/Scripts/Slingshot.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slingshot : MonoBehaviour
+{
+    static private Slingshot S;
+    [Header("Set in Inspector")]
+    public GameObject projectilePrefab;
+    public float velocityMult = 8f;
+    static public Vector3 LAUNCH_POS
+    {
+        get
+        {
+            if (S == null)
+                return Vector3.zero;
+            return S.launchPos;
+        }
+    }
+    [Header("Set Dynamically")]
+    public GameObject launchPoint;
+    public Vector3 launchPos;
+    public GameObject projectile;
+    public bool aimingMode;
+    public Rigidbody projectileRigidbody;
+    private void Awake()
+    {
+        S = this;
+        Transform launchPointTrans = transform.Find("LaunchPoint");
+        launchPoint = launchPointTrans.gameObject;
+        launchPoint.SetActive(false);
+        launchPos = launchPointTrans.position;
+    }
+    private void OnMouseEnter()
+    {
+        //print("Slingshot: OnMouseEnter()");
+        launchPoint.SetActive(true);
+    }
+
+    private void OnMouseExit()
+    {
+        //print("Slingshot: OnMouseExit()");
+        launchPoint.SetActive(false);
+    }
+
+    private void OnMouseDown()
+    {
+        aimingMode = true;
+        projectile = Instantiate(projectilePrefab) as GameObject;
+        projectile.transform.position = launchPos;
+        projectileRigidbody = projectile.GetComponent<Rigidbody>();
+        projectileRigidbody.isKinematic = true;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!aimingMode)
+            return;
+        else
+        {
+            Vector3 mousePos2D = Input.mousePosition;
+            mousePos2D.z = Camera.main.transform.position.z;
+            Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
+            Vector3 mouseDelta = mousePos3D - launchPos;
+            float maxMagnitude = this.GetComponent<SphereCollider>().radius;
+            if (mouseDelta.magnitude > maxMagnitude)
+            {
+                mouseDelta.Normalize();
+                mouseDelta *= maxMagnitude;
+            }
+
+            Vector3 projPos = launchPos + mouseDelta;
+            projectile.transform.position = projPos;
+            if (Input.GetMouseButtonUp(0))
+            {
+                aimingMode = false;
+                projectileRigidbody.isKinematic = false;
+                projectileRigidbody.velocity = -mouseDelta * velocityMult;
+                FollowCam.POI = projectile;
+                projectile = null;
+                MissionDemolition.ShotFired();
+                ProjectileLine.S.poi = projectile;
+            }
+        }
+    }
+}
